e64f7ae8821b3f9b05084eb972b71a95d8138bd2
soledelgadillo <54250755+soledelgadillo@users.noreply.github.com>
2021-05-19 10:42:45 +0100

    Update complete.R
    Changed names of variables to avoid confusion

--- R Programming/Week 2/Scripts/complete.R
+++ R Programming/Week 2/Scripts/complete.R
@@ -1,13 +1,13 @@
 complete <- function(directory, id = 1 : 332) {
         index <- list.files(directory, full.names = TRUE)[id]
-        shine <- data.frame()
+        df <- data.frame()
         for (i in 1 : length(index)) {
-                boom <- read.csv(index[i])
-                boom <- complete.cases(boom)
-                smith <- sum(boom)
-                shine <- rbind(shine, smith)
+                b <- read.csv(index[i])
+                boom <- complete.cases(b)
+                s <- sum(b)
+                df <- rbind(df, s)
         }
-        info <- cbind(id, shine)
+        info <- cbind(id, df)
         names(info) <- c("id", "nobs")
         print(info)
 }
